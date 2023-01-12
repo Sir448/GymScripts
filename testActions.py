cc57942eb4fe7b30ab091c9e9e1933e91fbecf09
Sir448 <christopher.attai@gmail.com>
2023-01-12 18:49:55 -0500

    Test reading sheets through actions 2

--- testActions.py
+++ testActions.py
@@ -1,5 +1,6 @@
 from __future__ import print_function
 
+import glob
 import os.path
 
 from google.auth.transport.requests import Request
@@ -101,8 +102,9 @@ def main():
         if creds and creds.expired and creds.refresh_token:
             creds.refresh(Request())
         else:
+            credFileName = glob.glob("*.json")[0]
             flow = InstalledAppFlow.from_client_secrets_file(
-                'credentials.json', SCOPES)
+                credFileName, SCOPES)
             creds = flow.run_local_server(port=0)
         # Save the credentials for the next run
         with open('token.json', 'w') as token:
